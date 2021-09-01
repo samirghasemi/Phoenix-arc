682aa40074151ca988ad2abf9c114381d6dfd951
samirghasemi <sam.s.a.mirghasemi@gmail.com>
2021-09-01 12:16:26 +0430

    1.0.0

---
+++ lib/test_web/uploaders/avatar.ex
@@ -0,0 +1,23 @@
+defmodule Test.Avatar do
+  use Arc.Definition
+  use Arc.Ecto.Definition
+  def __storage, do: Arc.Storage.Local
+  @versions [:original]
+  # Whitelist file extensions:
+  def validate({file, _}) do
+    ~w(.jpg .jpeg .gif .png) |> Enum.member?(Path.extname(file.file_name))
+  end
+  # Override the storage directory:
+  def storage_dir(version, {file, scope}) do
+    "uploads/avatars/#{scope.id}"
+  end
+
+  def filename(version, {file, scope}) do
+    file_name = Path.basename(file.file_name, Path.extname(file.file_name))
+    "#{scope.id}_#{version}_#{file_name}"
+  end
+  # Provide a default URL if there hasn't been a file uploaded
+  def default_url(version) do
+    TestWeb.Endpoint.url <> "/uploads/avatars/default.png"
+  end
+end
